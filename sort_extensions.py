9cf213d54b21af2a531e8996bb60d8b4ce0e0159
Daniel Cogiel <dcogiel.work@gmail.com>
2024-08-29 14:05:14 +0200

    Change of command line arguments handling

--- sort_extensions.py
+++ sort_extensions.py
@@ -1,6 +1,6 @@
 import os
 import shutil
-import sys
+import argparse
 
 
 def extract_paths_with_extensions(folder_path):
@@ -26,19 +26,24 @@ def create_extension_tree(extensions, files, output_folder):
         print('Output folder of given name already exists.')
 
 
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print('No folder to sort has been specified.')
-        sys.exit(0)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--source', type=str, required=True, help='Directory to sort')
+    parser.add_argument('-o', '--output', type=str, required=False, help='Directory to which sorted files should be saved')
+    args = parser.parse_args()
 
-    files = extract_paths_with_extensions(folder_path=sys.argv[1])
+    files = extract_paths_with_extensions(folder_path=args.source)
     unique_extensions = set([extension for _, extension in files])
 
     create_extension_tree(
         extensions=unique_extensions,
         files=files,
-        output_folder=sys.argv[2] or 'output'
+        output_folder=args.output or 'output'
     )
+
+
+if __name__ == "__main__":
+    main()
 
 
 
